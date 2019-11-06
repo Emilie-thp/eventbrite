19ed26a637bc84c5d965b7dad54481dec59fd371
Emilie-thp <emilie.marjollet@orange.fr>
2019-11-06 19:16:49 +0100

    show user, new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,24 @@ class EventsController < ApplicationController
   end
 
   def new
+  	@event = Event.new
+  end
+
+  def create
+ 		@event = Event.new('start_date' => params[:start_date],
+                  			 'duration' => params[:duration],
+                  			  'title' => params[:title],
+                  			   'description' => params[:description],
+                  			    'price' => params[:price],
+                  			     'location' => params[:location],
+                  			 )
+    @event.admin_id = current_user.id
+   		if @event.save # essaie de sauvegarder en base 
+     		 flash[:success] = "Nouvel événement bien créé !"
+         render 'events/index'
+	    else
+   		   render "new"
+    	end
   end
 
   def show
